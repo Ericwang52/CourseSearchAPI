96842951511524dbac47ee60c45c7f2328e33047
Eric Wang <ericwang52@yahoo.ca>
2020-10-04 22:10:56 -0400

    fix

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Hello</p>"
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -29,11 +29,12 @@ def search():
     else:
         return "Error: No term field provided. Please specify an id."
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = GOOGLE_CHROME_BIN
+    chrome_options.add_argument('window-size=800x841')
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
     finaldict= {"edx":scrapedx(term, sale, 4, driver), "Udemy":scrapUdemy(term, sale, 4, driver), "Coursera":scrapCoursera(term, sale, 4, driver), "Udacity":scrapUdacity(term, sale, 4, driver)}
     return jsonify(finaldict)
 
